20ee1a758a2d07dacbdc35a3656d81384569cbf0
Amr Hassan <103396210+amrgelno@users.noreply.github.com>
2026-01-25 11:15:35 +0200

    UI performance
    UI performance

---
+++ remove any comment from page.js
@@ -0,0 +1,10 @@
+
+function removeCommentsFromScript(code) {
+  // Remove HTML comments first
+  code = code.replace(/<!--[\s\S]*?-->/g, '');
+  // Then remove JS comments
+  code = code.replace(/\/\*[\s\S]*?\*\//g, '');
+  code = code.replace(/\/\/.*$/gm, '');
+  return code.trim();
+}
+
